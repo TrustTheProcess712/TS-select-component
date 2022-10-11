cc2c8834c51c591b4410c084a00e96673eadc03b
TrustTheProcess712 <marshall1610@hotmail.co.uk>
2022-10-11 12:41:39 +0100

    adds styles to component

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,8 +1,9 @@
 import styles from "./select.module.css";
+import { useState } from "react";
 
 type SelectOptions = {
   label: string;
-  value: string;
+  value: any;
 };
 
 type SelectProps = {
@@ -12,10 +13,38 @@ type SelectProps = {
 };
 
 export function Select({ value, onChange, options }: SelectProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function clearOptions() {
+    onChange(undefined);
+  }
+
   return (
-    <>
-      <div className="conatiner">Wrong</div>
-      <div className={styles.container}>Right</div>
-    </>
+    <div
+      onBlur={() => setIsOpen(false)}
+      onClick={() => setIsOpen((prev) => !prev)}
+      tabIndex={0}
+      className={styles.container}
+    >
+      <span className={styles.value}>{value?.label}</span>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          clearOptions();
+        }}
+        className={styles["clear-btn"]}
+      >
+        &times;
+      </button>
+      <div className={styles.divider}></div>
+      <div className={styles.caret}></div>
+      <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
+        {options.map((option) => (
+          <li key={option.label} className={styles.option}>
+            {option.label}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }

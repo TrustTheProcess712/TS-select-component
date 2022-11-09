a167620baeac79eee0a46de88d1e29b8e16f0f7e
TrustTheProcess712 <marshall1610@hotmail.co.uk>
2022-11-09 16:19:49 +0000

    adds multiple select option must debug usRef issue

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,7 +1,8 @@
 import styles from "./select.module.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom/client";
 
-type SelectOptions = {
+export type SelectOption = {
   label: string;
   value: string | number;
 };
@@ -25,6 +26,7 @@ type SelectProps = {
 export function Select({ multiple, value, onChange, options }: SelectProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [highlightedIndex, setHighlightedIndex] = useState(0);
+  // const conatinerRef = useRef<HTMLDivElement>(null);
 
   function clearOptions() {
     multiple ? onChange([]) : onChange(undefined);
@@ -35,7 +37,7 @@ export function Select({ multiple, value, onChange, options }: SelectProps) {
       if (value.includes(option)) {
         onChange(value.filter((o) => o !== option));
       } else {
-        onChange([...valie, option]);
+        onChange([...value, option]);
       }
     } else {
       if (option !== value) onChange(option);
@@ -43,21 +45,69 @@ export function Select({ multiple, value, onChange, options }: SelectProps) {
   }
 
   function isOptionSelected(option: SelectOption) {
-    return multiple ? value.inclodes(option) : option === value;
+    return multiple ? value.includes(option) : option === value;
   }
 
   useEffect(() => {
     if (isOpen) setHighlightedIndex(0);
   }, [isOpen]);
 
+  // useEffect(() => {
+  //   const handler = (e: KeyboardEvent) => {
+  //     if (e.target != containerRef.current) return;
+  //     switch (e.code) {
+  //       case "Enter":
+  //      case "Space":
+  //         setIsOpen((prev) => !prev);
+  //         if (isOpen) selectOption(options[highlightedIndex]);
+  //         break;
+  //       case "ArrowUp":
+  //       case "ArrowDown":
+  //         if (!isOpen) {
+  //           setIsopen(true);
+  //           break;
+  //         }
+  //         const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1);
+  //         if (newValue >= 0 && newValue < options.length)
+  //           setHighlightedIndex(newValue);
+  //         break;
+  //       case "Escape":
+  //         setIsOpen(false);
+  //         break;
+  //     }
+  //   };
+  //   containerRef.current?.addEventListener("keydown", handler);
+
+  //   return () => {
+  //     containerRef.current?.removeEventListener("keydown", handler);
+  //   };
+  // }, [isOpen, highlightedIndex, options]);
+
   return (
     <div
+      // ref={containerRef}
       onBlur={() => setIsOpen(false)}
       onClick={() => setIsOpen((prev) => !prev)}
       tabIndex={0}
       className={styles.container}
     >
-      <span className={styles.value}>{value?.label}</span>
+      <span className={styles.value}>
+        {multiple
+          ? value.map((v) => (
+              <button
+                key={v.value}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  selectOption(v);
+                }}
+                className={styles["option-badge"]}
+              >
+                {v.label}
+                <span className={styles["remove-btn"]}>&times;</span>
+              </button>
+            ))
+          : value?.label}
+      </span>
       <button
         onClick={(e) => {
           e.stopPropagation();
